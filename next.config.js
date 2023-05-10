8cadbe6ba52800ea0947d20caa752ad18aa52b05
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-05-10 00:26:12 -0700

    created folder structer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [new URL(process.env.NEXT_PUBLIC_API_URL).hostname],
+  },
+  publicRuntimeConfig: {
+    api: process.env.NEXT_PUBLIC_API_URL,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NEXT_PUBLIC_API_URL + "/v2/:path*",
+      },
+    ];
+  },
+  i18n: {
+    locales: ["default", "fr", "es", "ar", "en", "it", "de", "nl"],
+    defaultLocale: "default",
+    localeDetection: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        OpenSans: ["Open Sans", "sans-serif"],
+        NunitoSans: ["Nunito Sans", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        white: {
+          main: "#FFFFFF",
+          off: "#F5F5F5",
+          cool: "#e6e6e6",
+          dark: "#E0E4EB",
+        },
+        black: {
+          main: "#000000",
+          off: "#15181E",
+          cool: "#1A1A1A",
+        },
+        brand: {
+          main: "#FF8501",
+          mainOff: "#FFB464",
+          mainCool: "#FFF4E5",
+          secondary: "#240046",
+          tertiary: "#442769",
+        },
+        grey: {
+          main: "#7E858B",
+          off: "#3D4852",
+          cool: "#F2F2F2",
+          dark: "#B3B3B3",
+        },
       },
     },
   },
   plugins: [],
-}
+};
