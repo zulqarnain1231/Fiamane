a91759dd5a95f122bd872c10e0b6bbe51e2d4f09
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-05-12 01:55:08 -0700

    completed departure and favourite page

---
+++ pages/departure.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Departure from "layouts/pages/Dashboard/Departure"
+
+function departure() {
+  return (
+    <Departure />
+  )
+}
+
+export default departure
---
+++ pages/favorite.tsx
@@ -0,0 +1,10 @@
+import Favorites from 'layouts/pages/Dashboard/Favourites'
+import React from 'react'
+
+function favourite() {
+  return (
+    <Favorites />
+  )
+}
+
+export default favourite
